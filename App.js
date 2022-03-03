c10e4de3008e909545296758249592372637375c
brijk123k <brijk247@gmail.com>
2022-03-03 23:32:31 +0530

    Slider project

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Slider from './component/Slider/Slider'
+import "./component/Slider/Slider.css"
+
+
+function App() {
+  return (
+    <>
+      <Slider />
+    </>
+  );
+}
+
+export default App;
